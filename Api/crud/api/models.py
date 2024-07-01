bcde52a4bbd83476575103f9ed7e66c75b57d654
najeeb67 <149230449+najeeb67@users.noreply.github.com>
2024-07-01 20:33:34 +0500

    add projects data

--- Api/crud/api/models.py
+++ Api/crud/api/models.py
@@ -25,3 +25,4 @@ class Projects(models.Model):
     
     def __str__(self):
         return self.project_name
+
--- Api/crud/api/views.py
+++ Api/crud/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse , HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
-from .models import Employee
+from .models import Employee , Projects
 from django.shortcuts import get_object_or_404 
 
 @csrf_exempt
@@ -84,3 +84,40 @@ def delete_employe(request, employee_id):
         except Employee.DoesNotExist:
             return JsonResponse({"error": "Employee Not Found"}, status=404)
     return HttpResponseBadRequest("Method invaild")
+
+
+@csrf_exempt
+def add_project(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data.get("project_name")
+        description = data.get("project_description")
+        manager = data.get("project_manager")
+        start_date = data.get("project_start_date")
+        end_date = data.get("project_end_date")
+        
+        if Projects.objects.filter(name = name).exists():
+            return JsonResponse({"message":"Project Already exists"}, status = 201)
+        
+        user = Projects.objects.create(name = name , description = description , manager = manager , start_project = start_date , end_project = end_date)
+        user.save()
+        return JsonResponse({"message":"Project Added Sucessfully"}, status = 400)
+    return HttpResponse("invaild methods")
+
+
+def get_object(request, project_id):
+    if request.method == "GET":
+        try:
+            data = get_object_or_404(Projects, id = project_id)
+            project_data = {
+            "name" : data.project_name,
+            "description":data.project_description,
+            "manager":data.project_manager,
+            "start_date": data.project_start_date,
+            "end_date": data.project_end_date,
+            }
+            return JsonResponse(project_data, status = 201)
+        except Projects.DoesNotExist:
+            return JsonResponse({"message":"Project not found"})
+        
+    return HttpResponse("invaild methods")
